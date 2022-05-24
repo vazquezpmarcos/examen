d68efefd62d39ccd50d26c14b86b2b5b85750434
vazquezpmarcos <vazquezpmarcos@gmail.com>
2022-05-24 12:51:53 +0200

    Me rindo porque form.js ha dejado de actualizarse por motivos desconocidos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import renderForm from './Components/login';
 import renderHeader from "./Components/header"
 import renderFooter from "./Components/footer"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  [renderForm,
-  renderHeader,
+  [renderHeader,
+  renderForm,
   renderFooter]
 );
